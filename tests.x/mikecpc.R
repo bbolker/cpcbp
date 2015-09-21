a4fde7a8018baeadafa12ad23a5e5d6278881ebf
mikelin <lim88@mcmaster.ca>
2015-09-20 23:06:05 -0500

    mikecpc works on windows but not linux, maybe quotation error?!? fullcpc new wrapper to replicate phillips testmat of chisq test need to add df and pvals need to check with flury and phillips again

--- tests.x/mikecpc.R
+++ tests.x/mikecpc.R
@@ -1,24 +1,57 @@
+library(cpcbp)
+source('test_examples.R')
+
 mikecpc <- function(covs,npts,ncp=(ncol(covs[[1]])-1)){
   mlist <- list()
   mlist$evecs <- partial_cpc(covs,npts,ncp)
   mlist$evals <- list()
   mlist$cov <- list()
   ncov <- length(covs)
-  for(j in 1:ncov){ mlist$evals[[j]] <- diag(t(mlist$evecs[[j]]) %*% covs[[j]] %*% mlist$evecs[[j]])
-  mlist$cov[[j]] <- mlist$evecs[[j]] %*% diag(mlist$evals[[j]])  %*% t(mlist$evecs[[j]])
-  }
-  mlist$chisqtest <- list()
-  mlist$chisqtest$df <- (ncp+1)*(ncp)*(ncov-1)/2
-  mlist$chisqtest$chisq <- cpcchisq(mlist$cov,covs,npts)
+  for(j in 1:ncov){ 
+    ## Flury eq. 1.20, p. 70; cov[[j]]==S_j
+    mlist$evals[[j]] <- diag(t(mlist$evecs[[j]]) %*% covs[[j]] %*% mlist$evecs[[
+j]])
+    mlist$cov[[j]] <- mlist$evecs[[j]] %*% diag(mlist$evals[[j]])  %*% t(mlist$e
+vecs[[j]])}
   return(mlist)
 }
 
-
 cpcchisq <- function(cov1,cov2,npts){
-  num <- lapply(cov1,det)
-  dem <- lapply(cov2,det)
-  ll <- Reduce("+",mapply("*",lapply(mapply("/",num,dem),log),(npts-1)))
+  num <- sapply(cov1,det)
+  dem <- sapply(cov2,det)
+  ll <- sum((npts-1)*log(num/dem))  ## n or n-1?
+  ## ll <- Reduce("+",mapply("*",lapply(mapply("/",num,dem),log),(npts-1)))
   return(ll)
+}
+
+fullcpc <- function(covs,npts,ncp=(ncol(covs[[1]])-1)){
+  cpclist <- list()
+  n <- ncol(covs[[1]])
+  for(i in 1:(n-1)){
+    cpclist[[i]] <- mikecpc(covs,npts,ncp = i)
+  }
+  
+  mlist <- list()
+  mlist$evecs <- cpclist[[ncp]]$evecs
+  mlist$evals <- cpclist[[ncp]]$evals
+  mlist$cov <- cpclist[[ncp]]$cov
+#  mlist$chisqtest <- matrix(1,nrow=ncp,ncol=3)
+  cpclist[[n]] <- NA
+  cpclist[[n]]$cov <- covs
+  if(ncp == 1){
+    mlist$chisqtest <- matrix(c(cpcchisq(cpclist[[ncp]]$cov,cpclist[[n]]$cov,npts), 
+  "df","pval"),nrow=1,ncol=3)}
+  if(ncp > 1){
+    mlist$chisqtest <- matrix(1,nrow=ncp,ncol=3)
+    mlist$chisqtest[,2] <- "df"
+    mlist$chisqtest[,3] <- "pval"
+    for(i in 1:(ncp-1)){
+      mlist$chisqtest[i,1] <-  cpcchisq(cpclist[[ncp]]$cov,cpclist[[ncp-i]]$cov,npts)
+    }
+    mlist$chisqtest[ncp,1] <- cpcchisq(cpclist[[ncp]]$cov,cpclist[[n]]$cov,npts)} 
+                            
+  
+  return(mlist)
 }
 
 mikepoolcpc <- function(covs,npts){
@@ -40,7 +73,7 @@ mikebp <- function(x,f,evec, center = TRUE)if (missing(evec)) {
   }
   
   covs <- lapply(split(data.frame(x),f),cov)
-  evecs = mikecpc(covs,table(f))$evecs[[1]]
+  evecs = fullcpc(covs,table(f))$evecs[[1]]
   if (length(evecs) == 1 && is.na(evecs)) 
     return(NA)
   evec = evecs[, 1]
