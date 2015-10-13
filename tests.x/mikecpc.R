2e0be4c805a6f89f0e6451b4392c2d9eed96401c
mikelin <lim88@mcmaster.ca>
2015-10-13 15:25:07 -0500

    clean up

--- tests.x/mikecpc.R
+++ tests.x/mikecpc.R
@@ -1,16 +1,33 @@
 library(cpcbp)
 source('test_examples.R')
+source('ordering.R')
 
-mikecpc <- function(covs,npts,ncp=(ncol(covs[[1]])-1)){
+cpc <- function(cov,n){
+  common <- nrow(cov[[1]])
+  return(partial_cpc(cov,n,q=common -1,B=(diag(common))))
+}
+
+
+cpcchisq <- function(cov1,cov2,npts){
+  num <- sapply(cov1,det)
+  dem <- sapply(cov2,det)
+  ll <- sum((npts-1)*log(num/dem))  ## n or n-1?
+  return(ll)
+}
+
+cpc_object <- function(covs,npts,ncp=NULL){
+  if(is.null(ncp))(ncp=nrow(covs[[1]])-1)
   mlist <- list()
-  p <- ncol(covs[[1]])
+  p <- nrow(covs[[1]])
   k <- length(covs)
-  mlist$evecs <- partial_cpc(covs,npts,ncp)
+  full <- cpc(covs,npts)
+  if(ncp == (p-1)){
+    mlist$evecs <- full
+    mlist$par <- p*(p-1)/2 + k*p}
+  else{mlist$evecs <- partial_cpc(covs,npts,q=ncp,B=cpc_object(covs,npts)$evecs[[1]])
+  mlist$par <- p*(p-1)/2 + k*p + (k-1)*(p-ncp)*(p-ncp-1)/2}
   mlist$evals <- list()
   mlist$cov <- list()
-  mlist$par <- p*(p-1)/2 + k*p + (k-1)*(p-ncp)*(p-ncp-1)/2
-  if(ncp == (p-1)){
-      mlist$par <- p*(p-1)/2 + p}
   ncov <- length(covs)
   for(j in 1:ncov){ 
     ## Flury eq. 1.20, p. 70; cov[[j]]==S_j
@@ -18,50 +35,31 @@ mikecpc <- function(covs,npts,ncp=(ncol(covs[[1]])-1)){
 j]])
     mlist$cov[[j]] <- mlist$evecs[[j]] %*% diag(mlist$evals[[j]])  %*%
         t(mlist$evecs[[j]])}
-  return(mlist)
+  return(reorder.ee(mlist))
 }
 
-cpcchisq <- function(cov1,cov2,npts){
-  num <- sapply(cov1,det)
-  dem <- sapply(cov2,det)
-  ll <- sum((npts-1)*log(num/dem))  ## n or n-1?
-  ## ll <- Reduce("+",mapply("*",lapply(mapply("/",num,dem),log),(npts-1)))
-  return(ll)
-}
-
-fullcpc <- function(covs,npts,ncp=(ncol(covs[[1]])-1)){
-  cpclist <- list()
-  k <-length(covs)
-  p <- ncol(covs[[1]])
-  arbpar <- k*( p*(p-1)/2 + p )
-  n <- ncol(covs[[1]])
-  for(i in 1:(n-1)){
-    cpclist[[i]] <- mikecpc(covs,npts,ncp = i)
-  }
-  
-  mlist <- list()
-  mlist$evecs <- cpclist[[ncp]]$evecs
-  mlist$evals <- cpclist[[ncp]]$evals
-  mlist$cov <- cpclist[[ncp]]$cov
-#  mlist$chisqtest <- matrix(1,nrow=ncp,ncol=3)
-  cpclist[[n]] <- NA
-  cpclist[[n]]$cov <- covs
-  if(ncp == 1){
-    mlist$chisqtest <- data.frame(statistic = cpcchisq(cpclist[[ncp]]$cov,cpclist[[n]]$cov,npts), 
-  df = arbpar - cpclist[[ncp]]$par, pval = NA)}
-  if(ncp > 1){
-    mlist$chisqtest <- data.frame(statistic=rep(1,ncp),df=NA,pval=NA)
-
-    for(i in 1:(ncp-1)){
-      mlist$chisqtest[i,1] <-  cpcchisq(cpclist[[ncp]]$cov,cpclist[[ncp-i]]$cov,npts)
-      mlist$chisqtest[i,2] <- cpclist[[ncp]]$par - cpclist[[ncp-i]]$par
+cpc.LRT <- function(cpc1,cpc2=NULL,covs,npts){
+    k <- length(covs)
+    p <- nrow(covs[[1]])
+    arbpar <- k*( p*(p-1)/2 + p)
+    chisqtest <- data.frame(
+        statistic = NA,
+        df = NA,
+        pval = NA
+    )
+    if(is.null(cpc2)){
+        chisqtest[1,1] <- cpcchisq(cpc1$cov,covs,npts)
+        chisqtest[1,2] <- arbpar - cpc1$par
+        chisqtest[1,3] <- 1 - pchisq(chisqtest[1,1],chisqtest[1,2])
     }
-    mlist$chisqtest[ncp,1] <- cpcchisq(cpclist[[ncp]]$cov,cpclist[[n]]$cov,npts)
-    mlist$chisqtest[ncp,2] <- arbpar - cpclist[[ncp]]$par} 
-                            
-  
-  return(mlist)
-}
+    else{
+        chisqtest[1,1] <- cpcchisq(cpc1$cov,cpc2$cov,npts)
+        chisqtest[1,2] <- cpc2$par - cpc1$par
+        chisqtest[1,3] <- 1 - pchisq(chisqtest[1,1],chisqtest[1,2])
+    }
+    return(chisqtest)
+ }
+
 
 mikepoolcpc <- function(covs,npts){
   mlist <- list()
--- tests.x/ordering.R
+++ tests.x/ordering.R
@@ -1,6 +1,3 @@
-source("mikecpc.R")
-ff <- fullcpc(test_marten,n_marten)
-ff2 <- fullcpc(test_marten,n_marten,2)
 hmean <- function(L) {
     ## harmonic mean of a list of vectors
     inv <- lapply(L,function(x) 1/x)
@@ -22,41 +19,3 @@ reorder.ee <- function(x) {
     return(x)
 }
 
-ff <- reorder.ee(ff)
-ff2 <- reorder.ee(ff2)
-
-## compute pairwise cosine distances among (C)PCs as a way
-## of testing this?
-    
-## part 2: try to figure out what's up with ordering
-## in the vole example?
-
-## Vole partial CPC (1 CPC)
-v1 <- fullcpc(test_vole,n_vole,1)
-
-## http://stackoverflow.com/questions/2535234/find-cosine-similarity-in-r
-cosineDist <- function(x){
-  as.dist(1 - x%*%t(x)/(sqrt(rowSums(x^2) %*% t(rowSums(x^2))))) 
-}
-
-cdist2 <- function(x,y) x %*% y / sqrt(x%*%x * y%*%y)
-cdistmat <- function(x,y) {
-    n1 <- ncol(x)
-    n2 <- ncol(y)
-    res <- matrix(NA,n1,n2)
-    for (i in 1:n1) {
-        for (j in 1:n2) {
-            res[i,j] <- cdist2(x[,i],y[,j])
-        }
-    }
-    res
-}
-tmpf <- function(i,j) {
-    round(abs(zapsmall(cdistmat(v1$evecs[[i]],v1$evecs[[j]]))),3)
-}
-tmpf(1,2)  ## very similar
-tmpf(1,3)  ## pretty similar
-tmpf(1,4)  ## flipped
-
-
-## Q: 
